62cc4115e0aafa6e30f058ef10cf58f16d4433e6
Ahmet Karadağ <akkaradaga@gmail.com>
2024-10-04 14:48:18 +0300

    Log out func added

--- Messenger/Controllers/ProfileViewController.swift
+++ Messenger/Controllers/ProfileViewController.swift
@@ -6,24 +6,64 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    let data = ["Log out"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
 
-        // Do any additional setup after loading the view.
+
+}
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource  {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let logOut = data[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = logOut
+        content.textProperties.adjustsFontSizeToFitWidth = true
+        content.textProperties.alignment = .center
+        content.textProperties.color = .blue
+        cell.contentConfiguration = content
+        return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let actionSheet = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Log out", style: .destructive, handler: { [weak self] _ in
+            guard let strongSelf = self else{
+                return
+            }
+            do{
+                try FirebaseAuth.Auth.auth().signOut()
+                
+                let vc = LoginViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                nav.modalPresentationStyle = .fullScreen
+                nav.navigationBar.tintColor = .green
+                strongSelf.present(nav, animated: true, completion: nil)
+            
+            }catch{
+                print("failed log out")
+            }
+        }))
+        actionSheet.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: nil))
+        present(actionSheet, animated: true)
 
+    }
+    
 }
